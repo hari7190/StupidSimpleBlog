235dc9d3b8516f7d13107e7cce4c483303c8872c
Hari M S <hari7190@gmail.com>
2018-03-12 06:18:53 -0700

    Not implemented use case and some CSS.

--- src/parser/deployToRemote.py
+++ src/parser/deployToRemote.py
@@ -1,6 +1,6 @@
 # author: Hari MS
 #
-# 
+#
 
 import json, urllib.request
 
--- web/resources/js/custom.js
+++ web/resources/js/custom.js
@@ -7,6 +7,22 @@ $(document).ready(function () {
 });
 
 function loadAlgo(e) {
-    $("#algo-div").load("snippets/py-" + e.originalEvent.srcElement.value + ".html", function() {});
-    hljs.initHighlighting();
+    toogleCodeContainer(false);
+
+    $("#algo-div").load("snippets/py-" + e.originalEvent.srcElement.value + ".html",
+        function (responseText, textStatus, req) {
+            if (textStatus == "error") {
+                $("#algo-div").html('#TBD');
+            } else {
+                hljs.initHighlighting();
+            }
+    });
+    toogleCodeContainer(true);
+}
+
+function toogleCodeContainer(show) {
+    if(show)
+        $("#code-container").removeClass('not-visible').addClass('visible');
+    else
+        $("#code-container").removeClass('visible').addClass('not-visible');
 }
